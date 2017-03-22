e7e25e52e0572986ec79ad2e0b99fb10ad266ba6
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-03-22 22:13:46 +0100

    Update sprichwort.js

--- ToadsworthBot/sprichwort.js
+++ ToadsworthBot/sprichwort.js
@@ -1,5 +1,5 @@
 var Utils = Java.type('io.github.toadsworthlp.toadsworthbot.Utils');
 var sprichworte = [];
 
-sprichworte = Utils.getContentFromURL("https://toadsworthlp.github.io/Files/sprichworte.txt", "Konnte die Sprichwort-Liste nicht abrufen.").split("\\R", -1);
+sprichworte = Utils.getContentFromURL("https://toadsworthlp.github.io/Files/sprichworte.txt", "Konnte die Sprichwort-Liste nicht abrufen.").match(/[^\r\n]+/g);
 context.getMessage().getChannel().sendMessage(sprichworte[Utils.randomNumber(0, sprichworte.length-1)]);

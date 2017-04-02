3051d04bd112d5889833eff25999ecda72b416f4
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-04-02 22:03:01 +0200

    Create restart.js

---
+++ ToadsworthBot/restart.js
@@ -0,0 +1,5 @@
+var System = 	Java.type('java.lang.System');
+var Runtime = Java.type('java.lang.Runtime');
+
+Runtime.getRuntime().exec("java -jar ToadsworthBot-preconfigured.jar MjgzMjc2NDQ3NzUyMTI2NDY1.C4y9Cg.i_6K4kEmkrmpxW65a0pEAs6CbxY https://toadsworthlp.github.io/ToadsworthBot/toadsworthbot.cfg");
+System.exit(0)

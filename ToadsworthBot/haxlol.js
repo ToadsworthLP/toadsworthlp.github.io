547c64184e8cf8734aba75f3efd1898bcb9b240e
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-04-03 21:25:08 +0200

    Update haxlol.js

--- ToadsworthBot/haxlol.js
+++ ToadsworthBot/haxlol.js
@@ -2,4 +2,4 @@ var System = Java.type('java.lang.System');
 var Runtime = Java.type('java.lang.Runtime');
 
 Runtime.getRuntime().exec("echo Test");
-context.getMessage().getChannel().sendMessage("Neue Instanz des Bots erstellt.");
+context.getMessage().getChannel().sendMessage("RPC gesendet.");

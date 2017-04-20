d0d9e47d3040c493c00e3070c3f970b9e164925d
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-04-20 17:34:38 +0200

    Update restart.js

--- ToadsworthBot/restart.js
+++ ToadsworthBot/restart.js
@@ -1,5 +1,5 @@
 var System = 	Java.type('java.lang.System');
 var Runtime = Java.type('java.lang.Runtime');
 
-//Runtime.getRuntime().exec("java -jar ToadsworthBot-preconfigured.jar MjgyOTY0MzgyMzY1OTA5MDAy.C4uLWw.bYD4whCZ7qsoCoJZSc4oAQIQOVQ https://toadsworthlp.github.io/ToadsworthBot/toadsworthbot.cfg");
+//The exit code is checked by a shell script that relaunches the program when it's not 0
 System.exit(1);

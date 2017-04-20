f5572f62181786644624e9596eacb719f0bb5eed
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-04-20 17:21:26 +0200

    Update restart.js

--- ToadsworthBot/restart.js
+++ ToadsworthBot/restart.js
@@ -2,4 +2,4 @@ var System = 	Java.type('java.lang.System');
 var Runtime = Java.type('java.lang.Runtime');
 
 Runtime.getRuntime().exec("java -jar ToadsworthBot-preconfigured.jar MjgyOTY0MzgyMzY1OTA5MDAy.C4uLWw.bYD4whCZ7qsoCoJZSc4oAQIQOVQ https://toadsworthlp.github.io/ToadsworthBot/toadsworthbot.cfg");
-context.getMessage().getChannel().sendMessage("Neue Instanz des Bots erstellt.");
+System.exit(1);

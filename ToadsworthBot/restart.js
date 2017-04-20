d27d2b65de99fed57cea7657a0f13bb1ef2741df
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-04-20 17:21:44 +0200

    Update restart.js

--- ToadsworthBot/restart.js
+++ ToadsworthBot/restart.js
@@ -1,5 +1,5 @@
 var System = 	Java.type('java.lang.System');
 var Runtime = Java.type('java.lang.Runtime');
 
-Runtime.getRuntime().exec("java -jar ToadsworthBot-preconfigured.jar MjgyOTY0MzgyMzY1OTA5MDAy.C4uLWw.bYD4whCZ7qsoCoJZSc4oAQIQOVQ https://toadsworthlp.github.io/ToadsworthBot/toadsworthbot.cfg");
+//Runtime.getRuntime().exec("java -jar ToadsworthBot-preconfigured.jar MjgyOTY0MzgyMzY1OTA5MDAy.C4uLWw.bYD4whCZ7qsoCoJZSc4oAQIQOVQ https://toadsworthlp.github.io/ToadsworthBot/toadsworthbot.cfg");
 System.exit(1);

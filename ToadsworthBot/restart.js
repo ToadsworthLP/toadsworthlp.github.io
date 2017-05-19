5e1225a247b1cc8beaec7052480f61d09801f667
ToadsworthLP <fiery.toadsworth@gmail.com>
2017-05-19 18:16:27 +0200

    Delete restart.js

--- ToadsworthBot/restart.js
+++
@@ -1,5 +0,0 @@
-var System = 	Java.type('java.lang.System');
-var Runtime = Java.type('java.lang.Runtime');
-
-//The exit code is checked by a shell script that relaunches the program when it's not 0
-System.exit(1);
